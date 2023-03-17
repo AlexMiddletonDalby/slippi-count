1a21a002eb9d26366e8b008689961b4f5e2ab624
Alex Middleton-Dalby <alex.middletondalby@focusrite.com>
2023-03-17 16:55:32 +0000

    Parse args and look for games with connect code

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,19 @@
 use std::fs;
 use std::io;
 
+use clap::Parser;
+
 use peppi::model::frame::Frame;
 use peppi::model::game::Frames;
 use peppi::model::game::Player;
 
-fn count_lcancels(frames: Vec<Frame<2>>, player_index: usize) -> i32 {
-    let mut l_cancel_count: i32 = 0;
+#[derive(Parser)]
+struct Args {
+    #[arg(short, long)]
+    directory: String,
 
-    for curent_frame in frames {
-        let frame_data = curent_frame.ports[player_index].leader;
-
-        let l_cancel_data = frame_data.post.l_cancel;
-        if let Some(l_cancel) = l_cancel_data {
-            if let Some(l_cancel_successful) = l_cancel {
-                if l_cancel_successful {
-                    l_cancel_count += 1;
-                }
-            }
-        }
-    }
-
-    return l_cancel_count;
+    #[arg(short, long)]
+    connect_code: String,
 }
 
 fn get_player_index_for_connect_code(connect_code: String, players: &Vec<Player>) -> Option<usize> {
@@ -36,12 +28,8 @@ fn get_player_index_for_connect_code(connect_code: String, players: &Vec<Player>
     return None;
 }
 
-fn main() {
-    let connect_code = "MD#1"; //this should be specified by the user
-
-    //Todo: Find all files in directory and do this for each
-    let file = fs::File::open("src/test-slippi-files/Day 3-Game_20210718T092212.slp")
-        .expect("File could not be read");
+fn does_game_feature_connect_code(file: String, connect_code: &String) -> bool {
+    let file = fs::File::open(file).expect("File could not be read");
 
     let mut buffer = io::BufReader::new(file);
     let game = peppi::game(&mut buffer, None, None).expect("File could not be parsed");
@@ -49,24 +37,34 @@ fn main() {
     let found_player_index =
         get_player_index_for_connect_code(connect_code.to_owned(), &game.start.players);
 
-    let mut total_l_cancels: i32 = 0;
+    return found_player_index.is_some();
+}
 
-    if let Some(player_index) = found_player_index {
-        println!("Found player with index {}", player_index);
+fn main() {
+    let args = Args::parse();
 
-        let frames = game.frames;
+    let directory = args.directory;
+    let connect_code = args.connect_code;
 
-        let mut lcancel_count: i32 = 0;
-        match frames {
-            Frames::P2(data) => lcancel_count = count_lcancels(data, player_index),
-            _ => println!("Ignored because wrong number of players"),
+    let files = fs::read_dir(directory).expect("Files could not be read from given directory");
+
+    let mut num_games_with_connect_code: i32 = 0;
+    for file in files {
+        if let Ok(file) = file {
+            let file_name = file
+                .path()
+                .into_os_string()
+                .into_string()
+                .expect("Invlaid path");
+
+            if does_game_feature_connect_code(file_name.to_owned(), &connect_code) {
+                num_games_with_connect_code += 1;
+            }
         }
-
-        println!("Number of successful l-cancels in game: {}", lcancel_count);
-        total_l_cancels += lcancel_count;
-    } else {
-        println!("Player with specified connect code not found. Skipping...")
     }
 
-    println!("Total number of successful l-cancels: {}", total_l_cancels);
+    println!(
+        "Number of games featuring specified connect-code: {}",
+        num_games_with_connect_code
+    );
 }
